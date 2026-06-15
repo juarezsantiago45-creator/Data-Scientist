2729b16ed12195721f469a92b2f98bcc0616bbcf
juarezsantiago45-creator <juarezsantiago45@gmail.com>
2026-06-15 18:49:55 -0300

    Create modulo2_unidad1_diseno.sql

---
+++ modulo2_unidad1_diseno.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE CliPro
+USE CliPro
+CREATE TABLE Clientes (
+IDCliente int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Nombre varchar(100) NOT NULL,
+Perfil_bio TEXT NOT NULL,
+fecha_registro DATE NOT NULL,
+);
+SELECT*FROM Clientes
+
+CREATE TABLE Productos (
+IDProducto int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Descripción varchar(255) NOT NULL,
+Precio DECIMAL(10,2) NOT NULL,
+esta_activo varchar(10)NOT NULL,
+);
+SELECT*FROM Productos
+
+CREATE TABLE Pedidos(
+IDPedido int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+Fecha_Pedido DATETIME NOT NULL,
+IdCliente int NOT NULL FOREIGN KEY REFERENCES Clientes(IDCliente),
+IDProducto int NOT NULL FOREIGN KEY REFERENCES Productos(IDProducto),
+);
+
+SELECT *FROM Pedidos
