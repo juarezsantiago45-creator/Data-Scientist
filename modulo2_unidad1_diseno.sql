5eda2f03c250777a400c200706c65149e67fbbf7
juarezsantiago45-creator <juarezsantiago45@gmail.com>
2026-06-15 18:54:45 -0300

    Update modulo2_unidad1_diseno.sql

--- modulo2_unidad1_diseno.sql
+++ modulo2_unidad1_diseno.sql
@@ -5,7 +5,7 @@ IDCliente int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 Nombre varchar(100) NOT NULL,
 Perfil_bio TEXT NOT NULL,
 fecha_registro DATE NOT NULL,
-);
+)
 SELECT*FROM Clientes
 
 CREATE TABLE Productos (
@@ -13,7 +13,7 @@ IDProducto int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 Descripción varchar(255) NOT NULL,
 Precio DECIMAL(10,2) NOT NULL,
 esta_activo varchar(10)NOT NULL,
-);
+)
 SELECT*FROM Productos
 
 CREATE TABLE Pedidos(
@@ -21,6 +21,6 @@ IDPedido int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 Fecha_Pedido DATETIME NOT NULL,
 IdCliente int NOT NULL FOREIGN KEY REFERENCES Clientes(IDCliente),
 IDProducto int NOT NULL FOREIGN KEY REFERENCES Productos(IDProducto),
-);
+)
 
 SELECT *FROM Pedidos
